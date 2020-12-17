53aea36d1adae3d949ce38bb4fb967bdd13a4d11
schaepher <schaepher@outlook.com>
2020-12-18 01:17:02 +0800

    termux-init.sh 添加 SSH key 初始化；将 nps 相关代码移动到 npc 脚本

--- scripts/termux-init.sh
+++ scripts/termux-init.sh
@@ -11,19 +11,15 @@ apt install vim openssh termux-api
 # SSH 公钥。貌似只能通过公钥登录 Termux 的 SSH。
 mkdir -p ~/.ssh
 touch ~/.ssh/authorized_keys
+printf "enter SSH public key: "
+read ssh_public_key
+echo ${ssh_public_key} >> ~/.ssh/authorized_keys
 
 # 把脚本放到桌面点击执行，需要安装 Termux:Widget
 # .shortcuts 目录下的脚本执行时会启动 Termux 会话，在前台展示
 # .shortcuts/tasks 里的脚本会在后台执行
 mkdir -p ~/.shortcuts
 mkdir -p ~/.shortcuts/tasks
-
-# nps 客户端
-echo """
-#!/data/data/com.termux/files/usr/bin/sh
-termux-wake-lock
-~/npc/npc -config ~/npc/conf/npc.conf
-""" > ~/.shortcuts/tasks/npc
 
 # SSH 服务端
 echo """
@@ -41,9 +37,3 @@ echo """
 cp ~/.shortcuts/tasks/sshd ~/.termux/boot/sshd
 rm ~/.shortcuts/tasks/sshd
 """ > ~/.shortcuts/tasks/start-sshd-onboot
-
-echo """
-#!/data/data/com.termux/files/usr/bin/sh
-cp ~/.shortcuts/tasks/npc ~/.termux/boot/npc
-rm ~/.shortcuts/tasks/npc
-""" > ~/.shortcuts/tasks/start-npc-onboot
