19b79bb76c2e7bb19e038efd2b04d54d9f177ab3
schaepher <schaepher@outlook.com>
2020-12-18 00:16:48 +0800

    解压完该项目的压缩包后，删除压缩包

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,7 @@
 
 curl -L -o scripts.zip https://github.com/schaepher/script-and-config/archive/main.zip 
 unzip scripts.zip
+rm scripts.zip
 mv script-and-config-main sac
 cd sac/scripts
 chmod +x *
